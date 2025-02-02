e9d238803cfba41d6b6623affc39d29c071bec8e
Mustafa <musti.ahmed789@gmail.com>
2025-02-02 00:44:38 -0500

    additional features

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,24 +2,33 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 
+interface HeaderProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
 const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
-      <Link href='/' className="md:flex-1">
-        <Image 
-          src="/assets/icons/logo.svg"
-          alt="Logo with name"
-          width={120}
-          height={32}
-          className="hidden md:block"
-        />
-        <Image 
-          src="/assets/icons/logo-icon.svg"
-          alt="Logo"
-          width={32}
-          height={32}
-          className="mr-2 md:hidden"
-        />
+      <Link href='/' className="md:flex-1 flex items-center">
+        <div className="hidden md:flex items-center gap-2">
+          <Image
+            src="/assets/icons/MyDocs.png"
+            alt="MyDocs Logo"
+            width={32}
+            height={32}
+          />
+          <span className="text-white text-xl font-semibold">MyDocs</span>
+        </div>
+        <div className="md:hidden">
+          <Image
+            src="/assets/icons/MyDocs.png"
+            alt="MyDocs Logo"
+            width={32}
+            height={32}
+            className="mr-2"
+          />
+        </div>
       </Link>
       {children}
     </div>
